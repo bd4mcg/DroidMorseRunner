7d7c019077fedd8bb3e5aba97ddb1ede23a1a590
unknown <bd4mcg@gmail.com>
2026-08-29 15:16:31 +0800

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE = 'web-morse-runner-v1';
+const APP_SHELL = ['./', './index.html', './style.css', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    }
+    return response;
+  })));
+});
